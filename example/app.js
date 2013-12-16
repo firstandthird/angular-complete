544c4bde660b1a17cce16e4a00fa5d4c5fdc6a1c
Troy Whiteley <troy@somanyscientists.com>
2013-12-16 14:24:38 -0800

    First version.

--- example/app.js
+++ example/app.js
@@ -1,5 +1,18 @@
-angular.module('example-app', []);
+angular.module('example-app', ['ftComplete']);
 
 var MainController = function($scope) {
+  $scope.car = '';
 
+  $scope.suggestions = [
+    'Acura', 'Audi', 'BMW', 'Cadillac',
+    'Chrysler', 'Dodge', 'Ferrari', 'Ford',
+    'GMC', 'Honda', 'Hyundai', 'Infiniti',
+    'Jeep', 'Kia', 'Lexus', 'Mini',
+    'Nissan', 'Porsche', 'Subaru', 'Toyota',
+    'Volkswagon', 'Volvo'
+  ];
+
+  $scope.$watch('car', function(val) {
+    console.log('changed to ' + val);
+  });
 };
