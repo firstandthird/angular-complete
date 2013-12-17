7cf691d766d72736e78ade39f947be084a462ddd
Troy Whiteley <troy@somanyscientists.com>
2013-12-16 18:19:09 -0800

    Added format support.

--- example/app.js
+++ example/app.js
@@ -3,20 +3,79 @@ angular.module('example-app', ['ftComplete']);
 var MainController = function($scope) {
   $scope.car = '';
 
-  $scope.query = [
-    'Acura', 'Audi', 'BMW', 'Cadillac',
-    'Chrysler', 'Dodge', 'Ferrari', 'Ford',
-    'GMC', 'Honda', 'Hyundai', 'Infiniti',
-    'Jeep', 'Kia', 'Lexus', 'Mini',
-    'Nissan', 'Porsche', 'Subaru', 'Toyota',
-    'Volkswagon', 'Volvo'
+  var cars = [
+    {
+      name: 'Acura'
+    }, {
+      name: 'Audi'
+    }, {
+      name: 'BMW'
+    }, {
+      name: 'Cadillac'
+    }, {
+      name: 'Chrysler'
+    }, {
+      name: 'Dodge'
+    }, {
+      name: 'Ferrari'
+    }, { 
+      name: 'Ford' 
+    }, {
+      name: 'GMC' 
+    }, { 
+      name: 'Honda'
+    }, {
+      name: 'Hyundai'
+    }, {
+      name: 'Infiniti'
+    }, {
+      name: 'Jeep' 
+    }, {
+      name: 'Kia'
+    }, {
+      name: 'Lexus' 
+    }, {
+      name: 'Mini'
+    }, {
+      name: 'Nissan' 
+    }, {
+      name: 'Porsche'
+    }, {
+      name: 'Subaru'
+    }, {
+      name: 'Toyota'
+    }, {
+      name: 'Volkswagon'
+    }, {
+      name: 'Volvo'
+    }
   ];
 
-  $scope.$watch('car', function(val) {
-    console.log('changed to ' + val);
-  });
+  $scope.query = function(query, callback){
+    var queryLower = query.toLowerCase(), self = this;
+
+    var suggestions = [];
+
+    for(var car in cars) {
+      car = cars[car];
+
+      if(car.name.toLowerCase().indexOf(queryLower) !== -1) suggestions.push(car);
+    }
+
+    callback.apply(this,[suggestions]);
+  };
+
+  $scope.formatSuggestion = function(suggestion, value) {
+    var pattern = '(' + value + ')';
+    return suggestion.name.replace(new RegExp(pattern, 'gi'), '<strong>$1<\/strong>');
+  };
+
+  $scope.format = function(value) {
+    if(!value) return value;
+    return value.name;
+  };
 
   $scope.$on('complete:selected', function(e, val) {
-    console.log(val);
+    console.log('Changed to: ' + val.name, val);
   });
 };
