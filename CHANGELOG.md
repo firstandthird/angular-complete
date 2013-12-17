14599a48ce5d8bc29a78674de4ab471597534509
Troy Whiteley <troy@somanyscientists.com>
2013-12-16 18:21:19 -0800

    Release 0.1.0

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+
+0.1.0 / 2013-12-16 
+==================
+
+ * Added format support.
+ * Uses query if available.
+ * First version.
+ * project setup
---
+++ dist/angular-complete.js
@@ -0,0 +1,47 @@
+
+/*!
+ * angular-complete - Angular autocomplete directive
+ * v0.1.0
+ * https://github.com/firstandthird/angular-complete
+ * copyright First + Third 2013
+ * MIT License
+*/
+(function(){
+  angular.module('ftComplete', [])
+    .directive('complete', ['$parse', function($parse) {
+      return {
+        link: function(scope, el, attrs) {
+          var options = $parse(attrs.complete)() || {};
+
+          var format = scope.format || function(value) {
+            return value;
+          };
+
+          options.source = [];
+          if(typeof scope.query === 'function') {
+            options.query = scope.query;
+          } else {
+            options.source = scope.query;
+          }
+
+          if(typeof scope.formatSuggestion === 'function') {
+            options.formatSuggestion = scope.formatSuggestion;
+          }
+
+          options.format = format;
+
+          scope.complete = $(el).complete(options);
+
+          scope.complete.on('select', function(event, value){
+            if(!value) return;
+
+            scope.$apply(function(){
+              scope[attrs.ngModel] = format(value);
+            });
+
+            scope.$emit('complete:selected', value);
+          });
+        }
+      };
+    }]);
+}());
